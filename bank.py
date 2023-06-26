e4d28117d288f322306a0335dffa4d0e3c6e671b
test <test>
2023-06-26 18:53:05 +0700

    test

---
+++ bank.py
@@ -0,0 +1,11 @@
+answ = input("Приветствие: ")
+if answ == "здравствуйте":
+    print("$0")
+elif answ == "здравствуйте, человек":
+    print("$0")
+elif answ == "здрасте":
+    print("$20")
+elif answ == "привет":
+    print("$100")
+else:
+    print("$0")
